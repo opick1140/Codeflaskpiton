8b3152a3ecef4700d2c592ff373f5bd424286b1f
opick1140 <taupikf1140@gmail.com>
2025-01-17 21:39:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, opik!"
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
 
-import os
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(debug=True)
